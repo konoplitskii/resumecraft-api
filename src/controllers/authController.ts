9386b40d45690c44fe516e2e54c6ae459c9c349d
sv.konoplitskii <s.konoplitsky@yandex.com>
2025-11-04 20:30:43 +0300

    feat: test connect

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,7 +1,21 @@
 import { Request, Response } from 'express';
+import { pool } from '../db';
 export const registerUser = async (req: Request, res: Response) => {
-  console.log('req', req.body);
-  res
-    .status(201)
-    .json({ message: 'Пользователь создан', data: { result: true, userId: 'user.id' } });
+  const { name, email, password } = req.body;
+  try {
+    const result = await pool.query(
+      `INSERT INTO users (name, email, password)
+       VALUES ($1, $2, $3)
+       RETURNING *;`,
+      [name, email, password],
+    );
+
+    res.status(201).json({
+      message: '✅ User created successfully',
+      user: result.rows[0],
+    });
+  } catch (err: any) {
+    console.error('❌ Error creating user:', err);
+    res.status(500).json({ error: 'Internal Server Error', details: err.message });
+  }
 };
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from 'express';
 import authRouters from './routes/auth';
+import { createResumeTables } from './db/scripts';
 import dotenv from 'dotenv';
+import { pool } from './db';
 
 dotenv.config();
 
@@ -11,6 +13,18 @@ app.use(express.json());
 
 app.use('/api', [authRouters]);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+async function startServer() {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    console.log('✅ Connected to database at:', result.rows[0].now);
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Server is running on http://localhost:${PORT}`);
+    });
+  } catch (err) {
+    console.error('❌ Database connection failed:', err);
+    process.exit(1);
+  }
+}
+
+startServer();
